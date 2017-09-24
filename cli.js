a97a671a559c5b2775ced5619ff4965cc735a3cf
itaditya <adityaa803@gmail.com>
2017-09-25 01:26:01 +0530

    more condensed code

--- cli.js
+++ cli.js
@@ -1,24 +1,9 @@
 #!/usr/bin/env node
+
 'use strict';
 const meow = require('meow');
 const isPwa = require('./');
-
-const cli = meow([
-	'Usage',
-  '',
-  'Make sure you have ran `npm explore -g lighthouse -- npm run chrome&`',
-  '',
-	'  $ is-pwa [input]',
-	'',
-	'Examples',
-	'  $ is-pwa https://jsfeatures.in',
-  '   It is a PWA! With a lighthouse score of: 80',
-	'  ',
-	'  $ is-pwa https://h3manth.com',
-  '   Sorry, it\'s not a PWA :(',
-	'  '
-]);
-
+const cli = meow(['Usage', '', 'Make sure you have ran `npm explore -g lighthouse -- npm run chrome&`', '', '  $ is-pwa [input]', '', 'Examples', '  $ is-pwa https://jsfeatures.in', '   It is a PWA! With a lighthouse score of: 80', '  ', '  $ is-pwa https://h3manth.com', '   Sorry, it\'s not a PWA :(', '  ']);
 // Next version?
 //const spawn = require('child_process').spawn;
 // const chrome = spawn(
@@ -28,29 +13,25 @@ const cli = meow([
 //var exec = require('child_process').execSync
 //exec('./node_modules/lighthouse/lighthouse-core/scripts/launch-chrome.sh > /dev/null 2>&1 &');
 // exec('npm', 'explore -g lighthouse -- npm run chrome > /dev/null 2>&1 &'.split(' '))
-
-if(!cli.input[0]) {
+if (!cli.input[0]) {
   cli.showHelp();
 }
-
-isPwa(cli.input[0])
-  .then(score => {
-    if(score>75) {
-      console.log(`It is a PWA! With a lighthouse score of: ${score}`);
-    } else {
-      console.error('Sorry, it\'s not a PWA :(');
-    }
-    //chrome.kill('SIGHUP');
-  })
-  .catch(err => {
-    if (err.code === 'ECONNREFUSED') {
-      console.error('Unable to connect to Chrome. Please run Chrome w/ debugging port 9222 open:');
-      console.error('    npm explore -g lighthouse -- npm run chrome');
-      console.error('And then run:');
-      console.error(`is-pwa ${cli.input[0]}`);
-    } else {
-      console.error('Runtime error encountered:', err);
-      console.error(err.stack);
-    }
-    process.exit(1);
-  });
+isPwa(cli.input[0]).then(score => {
+  if (score > 75) {
+    console.log(`It is a PWA! With a lighthouse score of: ${score}`);
+  } else {
+    console.error('Sorry, it\'s not a PWA :(');
+  }
+  //chrome.kill('SIGHUP');
+}).catch(err => {
+  if (err.code === 'ECONNREFUSED') {
+    console.error('Unable to connect to Chrome. Please run Chrome w/ debugging port 9222 open:');
+    console.error('    npm explore -g lighthouse -- npm run chrome');
+    console.error('And then run:');
+    console.error(`is-pwa ${cli.input[0]}`);
+  } else {
+    console.error('Runtime error encountered:', err);
+    console.error(err.stack);
+  }
+  process.exit(1);
+});
--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 'use strict';
 const lighthouse = require('lighthouse');
-
 module.exports = url => {
     const getTotalScore = aggregation => {
-        const totalScore = aggregation.score.reduce((total, s) => {
-            return total + s.overall;
-        }, 0) / aggregation.score.length;
-
+        const totalScore = aggregation.score.reduce((total, s) => total + s.overall, 0) / aggregation.score.length;
         return Math.round(totalScore * 100);
     };
-
-    return lighthouse(url,  { mobile: true, loadPage: true })
-    .then(data => {
+    return lighthouse(url, {
+        mobile: true,
+        loadPage: true
+    }).then(data => {
         const agg = data.aggregations.find(a => a.name === 'Progressive Web App');
-        const totalScore = getTotalScore(agg);
-        return totalScore;
+        return getTotalScore(agg);
     });
 }
-
-
 // Origin -> https://github.com/paulirish/pwmetrics/commit/f71001ce3a9200015af07615e9834b222c4bc6d5#commitcomment-18754617
